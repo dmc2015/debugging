60190abf147d2c140d9d0d7dd9f6e20c534ebd93
jabyess <james.byess@gmail.com>
2018-06-05 11:48:05 -0400

    fix seed file

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const Tweet = require("../models/Tweet");
+const { Tweet } = require("../models/Tweet");
 const bcrypt = require("bcrypt-nodejs");
 
 const createPassword = password =>
