5f7829b2304ac8a12861978091ca4a5fef2c5230
Don McLamb <mclamb.donald@gmail.com>
2018-10-16 08:43:54 -0400

    modded connect

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,10 @@
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost/tweeter");
+// mongoose.Promise = Promise;
+// module.exports = mongoose;
+
+
+
 const mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost/tweeter");
 
@@ -6,8 +13,12 @@ const mongoose = require("mongoose");
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.MLAB_URL)
 } else {
-  mongoose.connect("mongodb://localhost/whenpresident");
+  mongoose.connect("mongodb://localhost/tweeter");
+
+  // mongoose.connect("mongodb://localhost/express-twitter");
+  // mongoose.connect('mongodb://admin2:admin2@ds253891.mlab.com:53891/test1');
 }
+// mongodb://<dbuser>:<dbpassword>@ds253891.mlab.com:53891/test1
 
 mongoose.Promise = Promise;
 module.exports = mongoose;
